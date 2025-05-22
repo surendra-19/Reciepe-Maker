50420f985ca28f20b361674fb4e1b92e34f81702
surendra-19 <surendrachagantipati@gmail.com>
2025-05-22 08:21:24 +0530

    changes

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -8,14 +8,16 @@ import {getRecipeFromMistral} from '../../Ai.js'
 
 function Main() {
     const [ingredients,setIngredients] = useState([]);
-    const [showReciepe,setShowReciepe] = useState(false);
+    const [Reciepe,setReciepe] = useState("");
     const ingredientsListItems = ingredients.map((ingredient)=><li key={ingredient}>{ingredient}</li>);
     function addIngredient(formData){
         const newIngredient = formData.get('ingredient')
         setIngredients((prev)=>[...prev,newIngredient])
     }
     async function loadReciepe(){
-        const recipe = getRecipeFromMistral(ingredients)
+        const recipeMarkdown = await getRecipeFromMistral(ingredients)
+        setReciepe(recipeMarkdown);
+        
     }
 
   return (
@@ -26,7 +28,7 @@ function Main() {
                 { ingredientsListItems.length > 0 && <IngredientsList ingredientsListItems = {ingredientsListItems}/> }
                 { ingredientsListItems.length > 3 && <GenerateReciepe loadReciepe = {loadReciepe} />  }
             </section> 
-            { showReciepe && <Reciepe/> }
+            { Reciepe && <Reciepe recipe={Reciepe} /> }
         </main>
     </>
   )
